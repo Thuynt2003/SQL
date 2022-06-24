4efcc565c155e0541774cacfddbf38f744903322
Thuynt2003 <101488871+Thuynt2003@users.noreply.github.com>
2022-06-24 13:17:57 +0700

    btvn

--- lab9.sql
+++ lab9.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE Lab11
+﻿CREATE DATABASE Lab11
 GO
 USE Lab11
 CREATE VIEW ProductList
@@ -15,6 +15,8 @@ JOIN AdventureWorks2019.Production.Product pr
 ON od.ProductID=pr.ProductID
 
 SELECT * FROM SalesOrderDetail
+
+/*------------------------------------bt tự làm lab9----------------------------------------------*/
 
 create table Customer(
 CustomerID int primary key,
@@ -87,3 +89,99 @@ JOIN Customer as c on c.CustomerID = bs.CustomerID
 JOIN Book as b on b.BookCode = bs.BookCode
 group by c.CustomerName
 select *from v_total_price
+
+/* ------------------------BTVN---------------------------------- */
+
+create table Class(
+ClassCode varchar(10) primary key,
+HeadTeacher varchar(30) ,
+Room varchar(30),
+TimeSlot char(10),
+CloseDate datetime
+)
+create table Student(
+RollNo varchar(10) primary key,
+ClassCode varchar(10),
+CONSTRAINT fk_ClassCode FOREIGN KEY (ClassCode)REFERENCES Class(ClassCode),
+FullName varchar(30),
+TimeSlot char(10),
+Male bit,
+BirthDate datetime,
+Address varchar(30),
+Email varchar(30)
+)
+create table Subjects(
+SubjectCode varchar(10) primary key,
+SubjectName varchar(40),
+WMark bit,
+PMark bit,
+WTest_per int,
+PTest_per int,
+Mark float
+)
+create table Mark(
+RollNo varchar(10),
+CONSTRAINT fk_RollNo FOREIGN KEY (RollNo)REFERENCES Student(RollNo),
+SubjectCode varchar(10),
+CONSTRAINT fk_SubjectCode FOREIGN KEY (SubjectCode)REFERENCES Subjects(SubjectCode),
+WMark float,
+PMark float,
+Mark float
+) 
+INSERT INTO Class(ClassCode,HeadTeacher,Room ,TimeSlot ,CloseDate)values
+('T2203M','Dang Kim Thi','B13','G','20240315'),
+('T2004M','Trinh Quang Hoa','B12','I','20220304'),
+('T1905M','Nguyen Tuan','B11','L','20210705'),
+('T1806M','Dao Hong Luyen','B10','M','20200808')
+
+INSERT INTO Student(RollNo,ClassCode,FullName,Male,BirthDate,Address,Email) values
+('A00261','T2203M','Nhu Tien Quyen',1,'20030102','HN','qwertyuio'),
+('A00262','T2004M','Đao Trong Quyet',0,'19900805','HP','mnbvcxfg'),
+('A00263','T1905M','Nguyen Huy Thang',1,'20030907','BN','hhgfdsss'),
+('A00264','T1806M','Nguyen Huu Thinh',0,'20001103','ND','hgfdsaczv')
+
+INSERT INTO Subjects(SubjectCode,SubjectName,WMark,PMark,WTest_per,PTest_per) values 
+('EPC','Elementary Programing with C',1,1,15,13),
+('HS','HTML&CSS',0,1,7,13),
+('JS','Javascrip',0,0,8,12),
+('SQL','Structured Query Language',1,0,12,7)
+INSERT INTO Mark(RollNo,SubjectCode,WMark,PMark,Mark)values
+('A00261','EPC',5.1,6,5.5),
+('A00261','HS',4,5.3,4.6),
+('A00262','EPC',3,5.4,4.2),
+('A00262','JS',5,7,6),
+('A00262','HS',7,8,7.5),
+('A00263','EPC',3,5,4),
+('A00264','EPC',5,6,5.5),
+('A00264','HS',7,8,7.5),
+('A00264','JS',8,9,8.5),
+('A00264','SQL',9,10,9.5)
+select *from Mark
+select *from Customer
+
+create view v_Student as
+select Student.FullName  from Student  join Mark on Student.RollNo=Mark.RollNo group by Student.FullName having COUNT(Mark.RollNo)>=2 
+select * from v_Student
+create view v_Students as
+select Student.FullName  from Student  join Mark on Student.RollNo=Mark.RollNo and PMark <5 or PMark<5
+select * from v_Students
+drop view IF EXISTS v_Students
+create procedure sp_Customers As Select  CustomerID,CustomerName,Address from Customer
+ExECUTE sp_Customers
+select *from BookSold
+drop procedure IF EXISTS sp_BookSold 
+create procedure sp_BookSold @Date int
+As
+Select BookSold.Date from BookSold where DATEPART(YY,Date) <= @Date
+EXECUTE sp_BookSold  2019
+
+
+
+create procedure sp_BookSoldCount @Date int,@Count int OUTPUT
+AS
+SELECT @Count=COUNT(*)from BookSold where DATEPART(YY,Date) >= @Date
+
+DECLARE @Number int 
+EXECUTE sp_BookSoldCount 2019 ,
+@Number OUTPUT
+PRINT @Number
